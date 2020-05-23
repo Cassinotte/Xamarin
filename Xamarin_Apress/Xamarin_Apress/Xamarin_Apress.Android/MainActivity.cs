e501b098513c512c0f7b7962760a644d2a1f3f7e
Eduardo Aparecido Cassinotte <ea.cassinotte@hotmail.com>
2020-05-23 15:36:40 -0300

    Apreendizado layout espeficos

--- Xamarin_Apress/Xamarin_Apress/Xamarin_Apress.Android/MainActivity.cs
+++ Xamarin_Apress/Xamarin_Apress/Xamarin_Apress.Android/MainActivity.cs
@@ -12,7 +12,15 @@ namespace Xamarin_Apress.Droid
     [Activity(Label = "Xamarin_Apress", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+
         protected override void OnCreate(Bundle savedInstanceState)
+        {
+            //OnCreateBase(savedInstanceState);
+            //OnCreateAndroid(Resource.Layout.LinearLayoutExample, savedInstanceState);
+            OnCreateAndroid(Resource.Layout.RelativeLayoutExample, savedInstanceState);
+        }
+
+        protected void OnCreateBase(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
@@ -23,6 +31,14 @@ namespace Xamarin_Apress.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+
+        protected void OnCreateAndroid(int layout, Bundle savedInstanceState)
+        {
+            RequestWindowFeature(WindowFeatures.NoTitle);
+            base.OnCreate(savedInstanceState);
+            SetContentView(layout);
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
