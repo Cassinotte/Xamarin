2603fe877e08e64a0939cc1ec50df400c4e2729d
Eduardo Aparecido Cassinotte <ea.cassinotte@hotmail.com>
2020-05-24 11:46:34 -0300

    Xamarin

--- Xamarin_Apress/Xamarin_Apress/Xamarin_Apress/Controls.cs
+++ Xamarin_Apress/Xamarin_Apress/Xamarin_Apress/Controls.cs
@@ -17,16 +17,37 @@ namespace Xamarin_Apress
 
             eventValue = new Label();
             eventValue.Text = "Value in Handler";
+            eventValue.FontSize = 20;
 
             pageValue = new Label();
             pageValue.Text = "Value in Page";
+            eventValue.FontSize = 20;
+
+            Picker picker = new Picker
+            {
+                Title = "Option",
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            var options = new List<string> { "Firts", "Second", "Third", "Fourth" };
+
+            foreach (var optionName in options)
+            {
+                picker.Items.Add(optionName);
+            }
+
+            picker.SelectedIndexChanged += (sender, args) =>
+            {
+                pageValue.Text = picker.Items[picker.SelectedIndex];
+            };
 
             Content = new StackLayout
             {
                 HorizontalOptions = LayoutOptions.Center,
                 Children = {
                     eventValue,
-                    pageValue
+                    pageValue,
+                    picker
                 }
             };
         }
