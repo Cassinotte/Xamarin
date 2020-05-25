46de0a6b27addcf2a6f96b03a4b33e6e805e9e92
Eduardo Aparecido Cassinotte <ea.cassinotte@hotmail.com>
2020-05-25 11:55:49 -0300

    Android component

---
+++ Xamarin_Apress/Xamarin_ControlExamplesAndroid/DateTimeActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Xamarin_ControlExamplesAndroid
+{
+    [Activity(Label = "DateTimeActivity")]
+    public class DateTimeActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.DateTime);
+
+            //DatePicker datePicker = FindViewById<DatePicker>(Resource.Id.edu);
+            TimePicker timePicker = FindViewById<TimePicker>(Resource.Id.timePicker);
+
+            timePicker.TimeChanged += delegate (object sender, TimePicker.TimeChangedEventArgs e)
+            {
+                Toast.MakeText(this, "Hour: " + e.HourOfDay + " Minute: " + e.Minute, ToastLength.Short).Show();
+            };
+        }
+
+    }
+}
--- Xamarin_Apress/Xamarin_ControlExamplesAndroid/MainActivity.cs
+++ Xamarin_Apress/Xamarin_ControlExamplesAndroid/MainActivity.cs
@@ -17,10 +17,10 @@ namespace Xamarin_ControlExamplesAndroid
 
             listItems = new List<ListItem> {
                 new ListItem {Title = "Spinner", PageType= typeof(SpinnerActivity)},
-                //new ListItem {Title = "Selection Controls", PageType= typeof(SelectionActivity)},
+                new ListItem {Title = "Selection Controls", PageType= typeof(SelectionActivity)},
                 new ListItem {Title = "Date Picker", PageType= typeof(DatePickerActivity)},
                 new ListItem {Title = "Time Picker", PageType= typeof(TimePickerActivity)},
-                //new ListItem {Title = "Date/Time Picker", PageType= typeof(DateTimeActivity)}
+                new ListItem {Title = "Date/Time Picker", PageType= typeof(DateTimeActivity)}
             };
 
             ListAdapter = new ListItemAdapter(this, listItems);
