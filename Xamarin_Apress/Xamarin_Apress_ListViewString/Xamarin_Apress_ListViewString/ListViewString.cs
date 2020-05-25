868fbdf36321260d3d1c11a485ccc755dfa0ce83
Eduardo Aparecido Cassinotte <ea.cassinotte@hotmail.com>
2020-05-25 15:06:27 -0300

    ListExample

---
+++ Xamarin_Apress/Xamarin_Apress_ListViewString/Xamarin_Apress_ListViewString/ListViewString.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Xamarin_Apress_ListViewString
+{
+    public class ListViewString : ContentPage
+    {
+        public ListViewString()
+        {
+            ListView listView = new ListView();
+
+            List<string> items = new List<string>() { "First", "Second", "Third" };
+
+            listView.ItemsSource = items;
+
+            listView.ItemTapped += async (sender, e) =>
+            {
+                await DisplayAlert("Tapped", e.Item.ToString() + "was selected.", "OK");
+                ((ListView)sender).SelectedItem = null;
+            };
+
+            listView.ItemSelected += async (sender, e) =>
+            {
+                if (e.SelectedItem == null) return;
+
+                await DisplayAlert("Selected", e.SelectedItem.ToString() + "was selected.", "OK");
+            };
+
+            this.Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
+
+            this.Content = new StackLayout {
+                Children = { listView }
+            
+            };
+        }
+    }
+}
