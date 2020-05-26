8a693b6118bad36c93e590602130c1e416a1e5f6
Eduardo Aparecido Cassinotte <ea.cassinotte@hotmail.com>
2020-05-26 16:21:14 -0300

    ListViewCustom

--- Xamarin_Apress/Xamarin_Apress_ListViewString/Xamarin_Apress_ListViewString/ListViewCustom.cs
+++ Xamarin_Apress/Xamarin_Apress_ListViewString/Xamarin_Apress_ListViewString/ListViewCustom.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -52,57 +53,112 @@ namespace Xamarin_Apress_ListViewString
                 ((ListView)sender).SelectedItem = null;
             };
         }
-    }
 
-    public class ListItemCell : ViewCell
-    {
-        public ListItemCell()
+        public class ListItemCell : ViewCell
         {
-            Label titleLabel = new Label
+            public ListItemCell()
             {
-                HorizontalOptions = LayoutOptions.FillAndExpand,
-                FontSize = 25,
-                FontAttributes = Xamarin.Forms.FontAttributes.Bold,
-                TextColor = Color.White
-            };
+                Label titleLabel = new Label
+                {
+                    HorizontalOptions = LayoutOptions.FillAndExpand,
+                    FontSize = 25,
+                    FontAttributes = Xamarin.Forms.FontAttributes.Bold,
+                    TextColor = Color.White
+                };
 
-            titleLabel.SetBinding(Label.TextProperty, "Title");
+                titleLabel.SetBinding(Label.TextProperty, "Title");
 
-            Label descLabel = new Label
-            {
-                HorizontalOptions = LayoutOptions.FillAndExpand,
-                FontSize = 12,
-                TextColor = Color.White
-            };
+                Label descLabel = new Label
+                {
+                    HorizontalOptions = LayoutOptions.FillAndExpand,
+                    FontSize = 12,
+                    TextColor = Color.White
+                };
 
-            descLabel.SetBinding(Label.TextProperty, "Description");
+                descLabel.SetBinding(Label.TextProperty, "Description");
 
-            StackLayout viewLayoutItem = new StackLayout()
-            {
-                HorizontalOptions = LayoutOptions.StartAndExpand,
-                Orientation = StackOrientation.Vertical,
-                Children = { titleLabel, descLabel }
-            };
+                StackLayout viewLayoutItem = new StackLayout()
+                {
+                    HorizontalOptions = LayoutOptions.StartAndExpand,
+                    Orientation = StackOrientation.Vertical,
+                    Children = { titleLabel, descLabel }
+                };
 
-            Label priceLabel = new Label
-            {
-                HorizontalOptions = LayoutOptions.End,
-                FontSize = 25,
-                TextColor = Color.Aqua
-            };
+                Label priceLabel = new Label
+                {
+                    HorizontalOptions = LayoutOptions.End,
+                    FontSize = 25,
+                    TextColor = Color.Aqua
+                };
 
-            priceLabel.SetBinding(Label.TextProperty, "Price");
+                priceLabel.SetBinding(Label.TextProperty, "Price");
 
-            StackLayout viewLayout = new StackLayout()
-            {
-                HorizontalOptions = LayoutOptions.StartAndExpand,
-                Orientation = StackOrientation.Horizontal,
-                Padding = new Thickness(25, 10, 55, 15),
-                Children = { viewLayoutItem, priceLabel }
+                StackLayout viewLayout = new StackLayout()
+                {
+                    HorizontalOptions = LayoutOptions.StartAndExpand,
+                    Orientation = StackOrientation.Horizontal,
+                    Padding = new Thickness(25, 10, 55, 15),
+                    Children = { viewLayoutItem, priceLabel }
 
-            };
+                };
 
-            View = viewLayout;
+                View = viewLayout;
+
+                var button = new Button
+                {
+                    Text = "Buy Now",
+                    BackgroundColor = Color.Teal,
+                    HorizontalOptions = LayoutOptions.EndAndExpand
+                };
+
+                button.SetBinding(Button.CommandParameterProperty, new Binding("."));
+
+                button.Clicked += (sender, e) =>
+                {
+                    var b = (Button)sender;
+                    var item = (ListItem)b.CommandParameter;
+
+                    ((ContentPage)this.Parent.Parent.Parent).DisplayAlert("Clicked", item.Title
+                    .ToString() + " button was clicked", "OK");
+
+                    //((ContentPage)((ListView)((StackLayout)((StackLayout)b.Parent)
+                    //.Parent).Parent).Parent).DisplayAlert("Clicked", item.Title
+                    //.ToString() + " button was clicked", "OK");
+
+
+                };
+
+                viewLayout.Children.Add(button);
+
+                var moreAction = new MenuItem { Text = "More" };
+
+                moreAction.SetBinding(MenuItem.CommandParameterProperty, new Binding("."));
+
+                moreAction.Clicked += (sender, e) =>
+                {
+                    var ml = ((MenuItem)sender);
+                    var item = (ListItem)ml.CommandParameter;
+
+                    Debug.WriteLine("More clicked on row: " + item.Title.ToString());
+                };
+
+                var deleteAction = new MenuItem { Text = "Delete", IsDestructive = true };
+
+                deleteAction.SetBinding(MenuItem.CommandParameterProperty, new Binding("."));
+
+                deleteAction.Clicked += (sender, e) =>
+                {
+                    var mi = ((MenuItem)sender);
+                    var item = ((ListItem)mi.CommandParameter);
+                    Debug.WriteLine("Delete clicked on row:" + item.Title.ToString());
+                };
+
+                ContextActions.Add(moreAction);
+                ContextActions.Add(deleteAction);
+
+            }
         }
     }
+
+    
 }
