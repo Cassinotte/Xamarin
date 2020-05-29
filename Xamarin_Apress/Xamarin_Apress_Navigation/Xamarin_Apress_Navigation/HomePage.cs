5821486c0c6e1514c76681bac7f4b2964e55a2b7
Eduardo Aparecido Cassinotte <ea.cassinotte@hotmail.com>
2020-05-29 16:27:13 -0300

    Navigation

---
+++ Xamarin_Apress/Xamarin_Apress_Navigation/Xamarin_Apress_Navigation/HomePage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Xamarin_Apress_Navigation
+{
+    public class HomePage : ContentPage
+    {
+        Button homeButton;
+
+        public HomePage()
+        {
+            Title = "Hierarchical Navigation";
+
+            Label homeLabel = new Label
+            {
+                Text = "Home Page",
+                FontSize = 40
+            };
+
+            homeButton = new Button
+            {
+                Text = "Go to Second Page"
+            };
+
+            homeButton.Clicked += async (sender, args) =>
+            await Navigation.PushAsync(new SecondPage());
+
+            StackLayout stackLayout = new StackLayout
+            {
+                Children = {
+                    homeLabel, homeButton
+                }
+            };
+
+            this.Content = stackLayout;
+        }
+    }
+}
---
+++ Xamarin_Apress/Xamarin_Apress_Navigation/Xamarin_Apress_Navigation/SecondPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Xamarin_Apress_Navigation
+{
+    public class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            Title = "Hierarchical Navigation";
+
+            Label homeLabel = new Label
+            {
+                Text = "Second Page",
+                FontSize = 40
+            };
+
+            var stackLayout = new StackLayout
+            {
+                Children = {
+                    homeLabel
+                }
+            };
+
+            this.Content = stackLayout;
+        }
+    }
+}
