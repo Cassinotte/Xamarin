936f68f5b8647362cd5b24519186e94bb4035e76
Eduardo Aparecido Cassinotte <ea.cassinotte@hotmail.com>
2020-06-08 17:07:34 -0300

    IOS

---
+++ Xamarin_Apress/Xamarin_NavigationIOS/AppDelegate.cs
@@ -0,0 +1,32 @@
+﻿using Foundation;
+using UIKit;
+
+namespace Xamarin_NavigationIOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : UIApplicationDelegate
+    {
+		// class-level declarations
+
+		UIWindow window;
+		public static UIStoryboard Storyboard = UIStoryboard.FromName("NavigationStory", null);
+		public static UIViewController firstViewController;
+
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+			window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+			firstViewController = Storyboard.InstantiateInitialViewController() as UIViewController;
+			UINavigationController navController = new UINavigationController(firstViewController);
+
+			window.RootViewController = navController;
+			window.MakeKeyAndVisible();
+
+			return true;
+		}
+	}
+}
+
+
